0c662c1ee173db192493d4922b771c9df3e18e32
Oyendrila Dobe <oyendrila.dobe@gmail.com>
2019-12-02 12:36:42 -0600

    Update 04-parametric-models.py

--- examples/parametric_models/04-parametric-models.py
+++ examples/parametric_models/04-parametric-models.py
@@ -21,7 +21,7 @@ def example_parametric_models_04():
     prism_program = stormpy.parse_prism_program(path)
 
     formula_str = "P=? [F s=7 & d=2]"
-    properties = stormpy.parse_properties(formula_str, prism_program)
+    properties = stormpy.parse_properties_for_prism_program(formula_str, prism_program)
     model = stormpy.build_parametric_model(prism_program, properties)
 
     # Modify
